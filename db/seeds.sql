565fe2bf15ea85dff0c508af488d405d16fd02c1
Megan Sleezer <64713309+mslee001@users.noreply.github.com>
2020-10-07 19:15:27 -0700

    Created server.js file and database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name,devoured) VALUES ("chicken", false);
+INSERT INTO burgers(burger_name,devoured) VALUES ("ground-beef", false);
+INSERT INTO burgers(burger_name,devoured) VALUES ("spaghetti", false);
+INSERT INTO burgers(burger_name,devoured) VALUES ("cheeseburger", false);
